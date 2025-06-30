68845d65c01ce6f75e7882f5b197fab104985f1c
ramimahammadarpon <ramim6708@gmail.com>
2025-06-30 19:17:09 +0600

    a bit modification

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -38,7 +38,7 @@ const Navbar = () => {
         return ()=> window.removeEventListener("scroll", handleScroll);
     }, [lastScrollY, isHidden]);
   return (
-    <div ref={navRef} className="navbar fixed top-0 bg-base-100 shadow-sm md:px-20 z-50">
+    <div ref={navRef} className="navbar fixed top-0 bg-base-100 shadow-sm lg:px-20 z-50">
       <div className="navbar-start">
         <div className="dropdown">
           <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
